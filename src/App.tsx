2ffaddd8ada03b7305ac2a64f251d1ee73a9a7a1
stack-guru <GoldenHand0302@gmail.com>
2025-06-03 16:46:54 -0500

    feat: initialize bridge-fe project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Button, HStack } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { Provider } from "@/components/ui/provider"
+
+createRoot(document.getElementById('root')!).render(
+  <Provider>
+    <App />
+  </Provider>
+)
