624434a5bdd3f44d4036dc093544aa3c1a69e398
stack-guru <GoldenHand0302@gmail.com>
2025-06-03 18:33:10 -0500

    feat: add base UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
 import { useState } from 'react'
-import { Button, HStack } from "@chakra-ui/react"
+import { Button, Box, Heading, Card, HStack, Center, Icon, Input, Field } from "@chakra-ui/react"
 
 function App() {
   return (
-    <>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
+    <Box padding={4}>
+      <Heading marginTop={8} size={"4xl"} textAlign={"center"}>Bridge Testing!</Heading>
+      <Box textAlign={"center"}>
+
+      </Box>
+      <HStack align={"center"} width={"full"} justifyContent={"center"} gap={8} marginTop={16}>
+        <Card.Root width={"520px"}>
+          <Card.Header>
+            <Card.Title mb="2">Get USDV on Solana</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Field.Root>
+              <Field.Label>
+                Solana Address <Field.RequiredIndicator />
+              </Field.Label>
+              <Input placeholder="Solana Address" />
+              <Field.HelperText>Solana Address</Field.HelperText>
+            </Field.Root>
+          </Card.Body>
+          <Card.Footer justifyContent={"center"}>
+            <Button marginTop={4}>Get</Button>
+          </Card.Footer>
+        </Card.Root>
+
+        <Card.Root width={"520px"}>
+          <Card.Header>
+            <Card.Title mb="2">Redeem USDV on Solana</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Field.Root>
+              <Field.Label>
+                Polygon Address <Field.RequiredIndicator />
+              </Field.Label>
+              <Input placeholder="Polygon Address" />
+              <Field.HelperText>Polygon Address</Field.HelperText>
+            </Field.Root>
+          </Card.Body>
+          <Card.Footer justifyContent={"center"}>
+            <Button marginTop={4}>Redeem</Button>
+          </Card.Footer>
+        </Card.Root>
       </HStack>
-    </>
+    </Box>
   )
 }
 
